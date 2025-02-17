f3b13004e866129fdc252c93225eee0c764067c1
cekharchandra-devops <cekharchandra.devops@gmail.com>
2025-02-17 21:18:46 +0530

    shell script

---
+++ 08-caller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+logDir="/var/log/hostname"
+
+export logDir
+
+chmod +x 08-calling.sh
+
+./08-calling.sh
---
+++ 09-calling.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "log dir: $logDir"
+
+echo "first param is: $1"
+echo "first param is: $2"
